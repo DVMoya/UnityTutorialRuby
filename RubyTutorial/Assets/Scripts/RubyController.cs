8497ec834b9bcd96a04cdba8171a9b4188c1603b
DVicente <90848834+DVMoya@users.noreply.github.com>
2021-11-24 10:31:34 +0100

    Fin ej2
    Con esto termino la segunda sesión de prácticas con unity.

--- RubyTutorial/Assets/Scripts/RubyController.cs
+++ RubyTutorial/Assets/Scripts/RubyController.cs
@@ -4,18 +4,42 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public int maxHealth = 5;
+
+    public float speed = 30.0f;
+
+    public int health { get { return currentHealth; }}
+    int currentHealth;
+    
+    Rigidbody2D rigidbody2d;
+    
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+       currentHealth = maxHealth;
+
+        currentHealth = 1;
     }
+
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+        
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+        
+        
+        rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
